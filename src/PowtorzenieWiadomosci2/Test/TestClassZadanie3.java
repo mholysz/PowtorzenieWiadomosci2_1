705d934d02222e69440c9a137d4038aa57a70b47
Maciek <maciejholysz@gmail.com>
2018-07-01 11:02:57 +0200

    Zadanie3 - no idea how to write test for Observer Pattern

--- src/PowtorzenieWiadomosci2/Test/TestClassZadanie3.java
+++ src/PowtorzenieWiadomosci2/Test/TestClassZadanie3.java
@@ -22,9 +22,8 @@ public class TestClassZadanie3 {
     }
 
     @Test
-    public void whenTwoSubscribersListInNewsBroadCaserLengthIsTwo(){
-        Subscriber subscriber3 = new Subscriber("Sub3", newsBroadcaster);
-
+    public void whenTwoSubscribersListInNewsBroadcasterLengthIsTwo(){
+        
     }
 
 }
